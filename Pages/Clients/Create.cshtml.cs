e7bc2916094e1146d097f2db5b6fbb5b57428e6d
paulaji <paulajiparayil123@gmail.com>
2023-06-24 17:36:50 +0530

    added the add new client razor page

---
+++ Pages/Clients/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebCRUD.Pages.Clients
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
