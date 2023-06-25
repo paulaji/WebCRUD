7f084c664274c67733531f2e47db1b3c84fee9fb
paulaji <paulajiparayil123@gmail.com>
2023-06-25 11:45:15 +0530

    added success or failed message to creation of client

--- Pages/Clients/Create.cshtml.cs
+++ Pages/Clients/Create.cshtml.cs
@@ -5,8 +5,28 @@ namespace WebCRUD.Pages.Clients
 {
     public class CreateModel : PageModel
     {
+        public ClientInfo clientInfo = new ClientInfo();
+        public String errorMessage = "";
+        public String successMessage = "";
         public void OnGet()
         {
+        }
+
+        public void OnPost() 
+        {
+            clientInfo.name = Request.Form["name"];
+            clientInfo.email = Request.Form["email"];
+            clientInfo.phone = Request.Form["phone"];
+            clientInfo.address = Request.Form["address"];
+
+            if(clientInfo.name.Length == 0)
+            {
+                errorMessage = "Name is required!";
+                return;
+            }
+
+            clientInfo.name = ""; clientInfo.email = ""; clientInfo.phone = ""; clientInfo.address = "";
+            successMessage = "Client added successfully ?";
         }
     }
 }
